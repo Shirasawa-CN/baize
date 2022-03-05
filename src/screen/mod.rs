7bbd590e351aef0ab57b68ecf243fe7e88ee1243
Cpointerz <3489840593@qq.com>
2022-03-05 08:50:03 +0800

    README&&SCREEN&&MESSAGE

--- src/screen/mod.rs
+++ src/screen/mod.rs
@@ -1,12 +1,14 @@
 use crate::message::*;
+extern crate term;
 
 //定义一个屏幕数据结构体
 pub struct Screen{
     width: usize,
     height: usize,
-    terimal: String,
     status: String,
     buffer: Vec<Vec<u128>>,
+    stream: Box<dyn term::Terminal<Output = std::io::Stdout> + Send>,
+    info: String,
 }
 
 pub struct Color{
@@ -15,7 +17,16 @@ pub struct Color{
 }
 
 impl Screen{
-    
+    pub fn new() -> Screen{
+        Screen{
+            width: 0,
+            height: 0,
+            status: String::new(),
+            buffer: Vec::new(),
+            info: String::new(),
+            stream: Box::from(term::stdout().unwrap()),
+        }
+    }
     fn get_size(&mut self) -> &str{
         if let Some((width, height)) = term_size::dimensions() {
             self.width = width;
@@ -23,7 +34,7 @@ impl Screen{
             SUCCESS
 
         } else {
-            ERROR_MSG_TERMINAL_IS_NOT_SUPPORTED
+            ERROR_MSG_TERMINAL_IS_NOT_WORKING
         }
     }
     fn check_size_error(&self) -> &str{
@@ -31,10 +42,28 @@ impl Screen{
             if width != self.width.try_into().unwrap() || height != self.height.try_into().unwrap() {
                 ERROR_MSG_SIZE_IS_NOT_CORRECT
             }else{
-                ERROR_MSG_TERMINAL_IS_NOT_SUPPORTED
+                ERROR_MSG_TERMINAL_IS_NOT_WORKING
             }
         } else {
-            ERROR_MSG_TERMINAL_IS_NOT_SUPPORTED
+            ERROR_MSG_TERMINAL_IS_NOT_WORKING
         }
+    }
+    fn out_put_to_screen(&self){
+        if self.status == "stop" {
+            loop{
+                if self.status == "start" {
+                    break;
+                }
+            }
+        }
+    }
+    pub fn print_welcome_info(&mut self){
+        self.stream.fg(term::color::WHITE).unwrap();
+        writeln!(self.stream, "{}", WELCOME_INFO).unwrap();
+    }
+    pub fn flush_screen(&mut self, msg: &str){
+        self.stream.fg(term::color::WHITE).unwrap();
+        writeln!(self.stream, "{}", msg).unwrap();
+        self.stream.flush().unwrap();
     }
 }
