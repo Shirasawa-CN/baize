2c434838c62cce0fa5dc443bdb6a467de86e8291
Cpointerz <3489840593@qq.com>
2022-03-09 23:19:28 +0800

    增加runtime

--- src/message/mod.rs
+++ src/message/mod.rs
@@ -1,5 +1,5 @@
 //欢迎信息
-pub const WELCOME_INFO: &'static str  = "
+pub const WELCOME_INFO: &str  = "
 Copyright (c) 2022 by Ruigang Zhang
 This Source Code Form is subject to the terms of the MIT LICENSE. 
 Baize is an experimental project for junior middle school students.
@@ -7,19 +7,17 @@ Because I'm a junior high school student, I can't fix and find bugs many times, 
 中考加油！
 ";
 
-//成功信息
-pub const SUCCESS: &'static str = "Success!";
 //错误信息
-pub const ERROR_MSG_SIZE_IS_NOT_CORRECT: &'static str = "Error:Size is not correct!";
-pub const ERROR_MSG_TERMINAL_IS_NOT_WORKING: &'static str = "Error:Terminal is not working!";
-pub const ERROR_MSG_CAN_NOT_OPEN_FILE: &'static str = "Error:Can not open file!";
-pub const ERROR_MSG_CAN_NOT_OPEN_DIR: &'static str = "Error:Can not open dir!";
-pub const ERROR_MSG_CAN_NOT_CREATE_DIR: &'static str = "Error:Can not create dir!";
-pub const ERROR_MSG_CAN_NOT_CREATE_FILE: &'static str = "Error:Can not create file!";
-pub const ERROR_MSG_CAN_NOT_DELETE_FILE: &'static str = "Error:Can not delete file!";
-pub const ERROR_MSG_CAN_NOT_DELETE_DIR: &'static str = "Error:Can not delete dir!";
-pub const ERROR_MSG_CAN_NOT_RENAME_FILE: &'static str = "Error:Can not rename file!";
-pub const ERROR_MSG_CAN_NOT_RENAME_DIR: &'static str = "Error:Can not rename dir!";
-pub const ERROR_MSG_CAN_NOT_MOVE_FILE: &'static str = "Error:Can not move file!";
-pub const ERROR_MSG_CAN_NOT_MOVE_DIR: &'static str = "Error:Can not move dir!";
-pub const ERROR_MSG_CAN_NOT_CONNECT_TO_LSP: &'static str = "Error:Can not connect to LSP!";
+pub const ERROR_MSG_SIZE_IS_NOT_CORRECT: &str = "Error:Size is not correct!";
+pub const ERROR_MSG_TERMINAL_IS_NOT_WORKING: &str = "Error:Terminal is not working!";
+pub const ERROR_MSG_CAN_NOT_OPEN_FILE: &str = "Error:Can not open file!";
+pub const ERROR_MSG_CAN_NOT_OPEN_DIR: &str = "Error:Can not open dir!";
+pub const ERROR_MSG_CAN_NOT_CREATE_DIR: &str = "Error:Can not create dir!";
+pub const ERROR_MSG_CAN_NOT_CREATE_FILE: &str = "Error:Can not create file!";
+pub const ERROR_MSG_CAN_NOT_DELETE_FILE: &str = "Error:Can not delete file!";
+pub const ERROR_MSG_CAN_NOT_DELETE_DIR: &str = "Error:Can not delete dir!";
+pub const ERROR_MSG_CAN_NOT_RENAME_FILE: &str = "Error:Can not rename file!";
+pub const ERROR_MSG_CAN_NOT_RENAME_DIR: &str = "Error:Can not rename dir!";
+pub const ERROR_MSG_CAN_NOT_MOVE_FILE: &str = "Error:Can not move file!";
+pub const ERROR_MSG_CAN_NOT_MOVE_DIR: &str = "Error:Can not move dir!";
+pub const ERROR_MSG_CAN_NOT_CONNECT_TO_LSP: &str = "Error:Can not connect to LSP!";
--- src/screen/mod.rs
+++ src/screen/mod.rs
@@ -6,7 +6,7 @@ pub struct Screen{
     width: usize,
     height: usize,
     status: String,
-    buffer: Vec<Vec<u128>>,
+    buffer: Box<Vec<String>>,
     stream: Box<dyn term::Terminal<Output = std::io::Stdout> + Send>,
     info: String,
 }
@@ -14,41 +14,43 @@ pub struct Screen{
 pub struct Color{
     themes: bool,
     themes_name: String,
+    themes_scheme: String,
+}
+
+pub struct Render{
+
 }
 
 impl Screen{
-    pub fn new() -> Screen{
+    pub fn new() -> Self{
         Screen{
             width: 0,
             height: 0,
             status: String::new(),
-            buffer: Vec::new(),
+            buffer: Box::new(Vec::new()),
             info: String::new(),
             stream: Box::from(term::stdout().unwrap()),
         }
     }
-    fn get_size(&mut self) -> &str{
+    fn get_size(&mut self){ 
         if let Some((width, height)) = term_size::dimensions() {
             self.width = width;
             self.height = height;
-            SUCCESS
-
-        } else {
-            ERROR_MSG_TERMINAL_IS_NOT_WORKING
         }
     }
-    fn check_size_error(&self) -> &str{
+    pub fn check_size_error(&mut self) -> Result<(),&str>{
         if let Some((width, height)) = term_size::dimensions() {
             if width != self.width || height != self.height {
-                ERROR_MSG_SIZE_IS_NOT_CORRECT
+                self.get_size();
+                Ok(()) 
             }else{
-                ERROR_MSG_TERMINAL_IS_NOT_WORKING
+                Err(ERROR_MSG_TERMINAL_IS_NOT_WORKING)
             }
         } else {
-            ERROR_MSG_TERMINAL_IS_NOT_WORKING
+            Err(ERROR_MSG_TERMINAL_IS_NOT_WORKING)
         }
     }
-    fn out_put_to_screen(&self){
+    pub fn out_put_to_screen(&self){
         if self.status == "stop" {
             loop{
                 if self.status == "start" {
@@ -65,5 +67,27 @@ impl Screen{
         self.stream.fg(term::color::WHITE).unwrap();
         writeln!(self.stream, "{}", self.info).unwrap();
         self.stream.flush().unwrap();
+    }
+    pub fn new_buffer(&mut self, member_number: String){
+        self.buffer.push(member_number); 
+    }
+}
+
+impl Color{
+    pub fn new() -> Self{
+        Color{
+            themes: false,
+            themes_name: String::from("none"),
+            themes_scheme: String::from("none"),
+        }
+    }
+    pub fn change_themes(&mut self, themes: bool){
+        self.themes = themes;
+    }
+    pub fn change_themes_name(&mut self, themes_name: String){
+        self.themes_name = themes_name;
+    }
+    pub fn change_themes_scheme(&mut self, themes_scheme: String){
+        self.themes_scheme = themes_scheme;
     }
 }
